92045cd24e7f523eb337d786d88f177c7753e31c
Prateek7071 <tiwariprateek123@gmail.com>
2026-03-22 21:29:13 +0530

    some basics

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
 import Mikser from "./Miks"
 
 function App() {
+  const username = "this is a variable"
   return (
     <>
-    <h1>Hello Vite App</h1>
+      <h1>Hello Vite App {username}</h1> // this {username} is called as evaluated expression, here we write the final evaluated value, you cant do evaluation here
     <Mikser />
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,51 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+export function Cobbler() {
+  return (
+    <div>
+      Hi this is custom
+    </div>
+  )
+}
+
+// at the end of the day the content inside of the cobbler funtion just get converted to this:
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://www.google.com',
+//     target: '_blank'
+//   },
+//   children: "Click to visit google"
+// }
+// but directly this wont work as the .render has some predefined parameters it takes
+
+// const anotherElement = (
+//   <a href="https://www.google.com" target='_blank'>Visit Google</a>
+// )
+
+const anotherUser = " This is where the variables go"
+const reactElem = React.createElement(
+  "a",
+  {
+    href: "https://www.google.com",
+    target:"_blank"
+  },
+  "Click me to visit google",
+  anotherUser
 )
+
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  
+  // Cobbler()
+  // anotherElement
+  reactElem
+  // <App /> 
+)
+
