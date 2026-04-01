bc02b2b38a0fd1bc1d2b84b55e3a7ce807740f82
Prateek7071 <tiwariprateek123@gmail.com>
2026-04-01 22:02:20 +0530

    created themeContext and custom themehook

---
+++ 09themeSwitcher/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { ThemeProvider } from "./contexts/theme";
+import { useEffect } from "react";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+  
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+  
+  //actually how the theme is changed
+  
+  useEffect(() => {
+    const Html = document.querySelector('html')
+    Html.classList.remove("light", "dark")
+    Html.classList.add(themeMode)
+  }, [themeMode])
+  
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/*theme btn*/}
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+          {/*Card*/}
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
