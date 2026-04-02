3a67e1c1b0d47c68c859d480947f8efd28209073
Prateek7071 <tiwariprateek123@gmail.com>
2026-04-02 20:22:21 +0530

    created todoForm and also used localStorage

--- 10todoContextLocalStorage/src/App.jsx
+++ 10todoContextLocalStorage/src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useState, } from "react";
 import "./App.css";
 import { TodoContext, TodoProvider } from "./contexts";
+import { useEffect } from "react";
 
 function App() {
-  const [todos, setTodos] = useState([])
+  // const [todos, setTodos] = useState([]) affected due to re-writing to incorporate localStorage
+  
+  const [todos, setTodos] = useState(() => {
+    try {
+      const savedTodos = localStorage.getItem("todos")
+      return savedTodos? JSON.parse(savedTodos) : []
+    } catch (error) {
+      console.log(error)
+      return []
+    }
+  })
   
   const addTodo = (todo) => {
     setTodos((prev)=> [{id:Date.now, ...todo} , ...prev])
@@ -21,6 +32,21 @@ function App() {
     setTodos(prev => prev.map(prevTodo=> prevTodo.id===id?{...prevTodo,completed:!prevTodo.completed}:prevTodo))
   }
   
+  //below is how sir told to get data from localstorage and render when we refresh or whenever website loads for the first time but now 2026 cant do useEffect(useState) cause of infinite render and all that so changing it above and calling it there.
+  
+  // useEffect(() => {
+  //   const todos = JSON.parse(localStorage.getItem("todos"))
+    
+  //   if (todos && todos.length > 0) {
+  //     setTodos(todos)
+  //   }
+    
+  // },[])
+  
+  
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
   
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
