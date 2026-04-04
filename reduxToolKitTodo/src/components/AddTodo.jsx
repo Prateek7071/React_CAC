247519d35cd833feedd01533416790827523cec6
Prateek7071 <tiwariprateek123@gmail.com>
2026-04-04 21:03:16 +0530

    added update functionality and changed todos functionality to display todos

--- reduxToolKitTodo/src/components/AddTodo.jsx
+++ reduxToolKitTodo/src/components/AddTodo.jsx
@@ -30,7 +30,7 @@ export default function AddTodo() {
       />
       <button
         type="submit"
-        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-base"
       >
         Add Todo
       </button>
