90ff98bd9faf6c9ae219006ff4a893d799077dda
Prateek7071 <tiwariprateek123@gmail.com>
2026-05-05 08:34:44 +0530

    created main

--- 12megaBlog/src/main.jsx
+++ 12megaBlog/src/main.jsx
@@ -6,12 +6,63 @@ import { Provider } from 'react-redux'
 import store from './store/store.js'
 
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { AuthLayout, Login} from './components/index.js'
 
+import { AddPost, AllPosts, Home, Signup, EditPost, Post } from './pages/index.js'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: <AuthLayout authentication={ false} >
+          <Login />
+        </AuthLayout>
+      },
+      {
+        path: '/signup',
+        element: <AuthLayout authentication={ false} >
+          <Signup />
+        </AuthLayout>
+      },
+      {
+        path: '/all-post',
+        element: <AuthLayout authentication="" >
+          <AllPosts />
+        </AuthLayout>
+      },
+      {
+        path: '/add-post',
+        element: <AuthLayout authentication={ true} >
+          <AddPost />
+        </AuthLayout>
+      },
+      {
+        path: '/edit-post/:slug',
+        element: <AuthLayout authentication={ true} >
+          <EditPost />
+        </AuthLayout>
+      },
+      {
+        path: '/post/:slug',
+        element: <AuthLayout authentication={ false} >
+          <Post />
+        </AuthLayout>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={router}/>
     </Provider>
   </StrictMode>,
 )
